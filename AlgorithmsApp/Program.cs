4682ca51788203500df04dc1494db3a887521ce9
Barry Wallis <barry.wallis@outlook.com>
2022-08-01 22:55:17 -0700

    Add BinarySearchApp

--- AlgorithmsApp/Program.cs
+++ AlgorithmsApp/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using AlgorithmsLib;
+
+int[] myList = new int[] { 1, 3, 5, 7, 9, };
+BinarySearch<int> binarySearch = new(myList);
+
+Console.WriteLine(ConvertToString(binarySearch.Search(3)));
+Console.WriteLine(ConvertToString(binarySearch.Search(-1)));
+
+string ConvertToString(int? value) => value.HasValue ? value.ToString() : "No Value";
